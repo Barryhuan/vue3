52f0bcd0f9d5e3a1ce16df1ea6d59027b78afef9
Barry焕 <13316348104@163.com>
2021-10-09 17:15:37 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import * as echarts from 'echarts'
 import ElementPlus from 'element-plus'
 import './assets/style/common.css'
 import 'element-plus/dist/index.css'
-const app = createApp(App)
-app.provide('$echarts', echarts)
-app
+import 'element-plus/theme-chalk/display.css'
+createApp(App)
   .use(router)
   .use(store)
   .use(ElementPlus)
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,21 +1,61 @@
 <style lang="less" scoped>
-  #login {
-    .login-container {
-      .wh(100%, 100%);
+  .login-container {
+    .wh(100%, 100%);
+    .nav {
+      .bd(b, 1px solid #dcdfe6);
+      .el-menu {
+        .h(55px);
+        li {
+          .h(54px);
+          .el-sub-menu__title{
+
+          }
+        }
+      }
     }
+  }
+  .el-sub-menu__title {
+    .h(54px);
   }
 </style>
 
 <template>
-  <div id="login" class="full-screen">
-    <el-container class="login-container">
-      <el-header></el-header>
-      <el-main ref="echartsId"></el-main>
-      <el-footer></el-footer>
-    </el-container>
-  </div>
+  <el-container class="login-container full-screen">
+    <el-header class="nav" height="55px">
+      <el-row :gutter="10" justify="space-between">
+        <el-col :span="4">
+          <div class="logo">
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <el-menu
+            :default-active="activeIndex"
+            @select="handleSelect"
+            mode="horizontal"
+            menu-trigger="click"
+          >
+            <el-menu-item index="1">指南</el-menu-item>
+            <el-menu-item index="2">组件</el-menu-item>
+            <el-menu-item index="3">资源</el-menu-item>
+             <el-sub-menu index="4">
+              <template #title>语言</template>
+              <el-menu-item index="4-1">English</el-menu-item>
+              <el-menu-item index="4-2">日本語</el-menu-item>
+              <el-menu-item index="4-3">简体中文</el-menu-item>
+            </el-sub-menu>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main></el-main>
+    <el-footer></el-footer>
+  </el-container>
 </template>
 
 <script setup>
-console.log(import.meta.env.DEV)
+  import { ref, reactive, toRefs } from 'vue'
+  let activeIndex = ref('1')
+  let handleSelect = (key, path) => {
+    console.log(key, path)
+  }
 </script>
